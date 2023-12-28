976c6e0bfc5c74f34a0018dd4de9e26d5e81212a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-12-27 19:12:39 -0500

    update

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,11 +1,23 @@
 // theme.service.ts
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
   private readonly THEME_KEY = 'appTheme';
+  currentTheme: string = 'light'; // default theme
+
+  darkThemeSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+
+  constructor(private themeService: ThemeService) {}
+
+  setDarkTheme(isDarkTheme: boolean) {
+    this.darkThemeSubject.next(isDarkTheme);
+  }
 
   setTheme(theme: string): void {
     localStorage.setItem(this.THEME_KEY, theme);

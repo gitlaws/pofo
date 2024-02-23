8b5545fa8068ba51726d2a81856a10f6b73f9fc8
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-02-22 19:41:16 -0500

    Refactor theme toggle component styles and update menu background color

--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -8,13 +8,18 @@ import { Theme } from 'src/app/services/theme.enum';
   styleUrls: ['./theme-toggle.component.scss'],
 })
 export class ThemeToggleComponent implements OnInit {
-  theme: Theme = Theme.Light; // Initialize theme with a default value
+  // theme: Theme = Theme.Light; // Initialize theme with a default value
+
+  // isDarkMode: boolean;
+  isDarkMode!: boolean;
+
+  isAnimated: boolean = false; // or true, depending on your default state
 
   constructor(private themeService: ThemeService) {}
 
   ngOnInit(): void {
     this.themeService.currentTheme.subscribe((theme) => {
-      this.theme = theme;
+      this.isDarkMode = theme === Theme.Dark;
     });
   }
 

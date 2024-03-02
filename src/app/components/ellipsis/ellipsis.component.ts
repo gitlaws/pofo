cf6a0cb341dc32a0d01d395909d2e10ca19618c1
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-02 14:40:51 -0500

    Refactor header component and toolmenu component

---
+++ src/app/components/ellipsis/ellipsis.component.ts
@@ -0,0 +1,42 @@
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Renderer2,
+  ElementRef,
+  HostBinding,
+  Input,
+} from '@angular/core';
+import { ThemeService } from 'src/app/services/theme.service';
+import { Theme } from 'src/app/services/theme.enum';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-ellipsis',
+  templateUrl: './ellipsis.component.html',
+  styleUrl: './ellipsis.component.scss',
+})
+export class EllipsisComponent {
+  currentTheme: string = '';
+  isDarkMode: boolean = false;
+
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(
+    private themeService: ThemeService,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
+  ngOnInit(): void {
+    this.themeService.currentTheme
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((theme) => {
+        this.isDarkMode = theme === Theme.Dark;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.unsubscribe();
+  }
+}

36e234729ecdee999e59edea68fc1b3392a43d26
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-07 19:31:46 -0500

    Refactor menu component and module***
    This commit refactors the menu component and module. Changes include:
    - Replacing the dropdown menu with a list of menu items
    - Using Angular's routerLink directive for navigation
    - Importing the RouterModule in the menu module

--- src/app/components/menu/menu.module.ts
+++ src/app/components/menu/menu.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Import RouterModule
 import { MenuComponent } from './menu.component';
 
 @NgModule({
   declarations: [MenuComponent],
+  imports: [
+    CommonModule,
+    RouterModule, // Add RouterModule to the imports array
+  ],
   exports: [MenuComponent],
-  imports: [CommonModule],
 })
 export class MenuModule {}
